06c78490b20bf32404bea649714cbd5f78729de1
Olecya <OlecyaMarkina@yandex.ru>
2023-03-12 23:32:58 +0300

    пр 13 add 404 page

--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,9 @@ const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.use('*', (req, res, next) => {
+    res.status(404).send({ message: '404 Not Found' });
+  });
+  
 
 module.exports = router;
