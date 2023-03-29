96ae84efbc7f834e7229a3fa2e61394c742e090e
Olecya <OlecyaMarkina@yandex.ru>
2023-03-29 23:14:42 +0300

    пр 14 v3 send JSON eslint fix

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use((err, req, res, next) => {
     error.statusCode = 500;
     error.message = 'Произошла ошибка сервера';
   }
-  const massageErr = error.message
-  res.status(error.statusCode).send({massageErr});
+  const massageErr = error.message;
+  res.status(error.statusCode).send({ massageErr });
   next();
 });
 
